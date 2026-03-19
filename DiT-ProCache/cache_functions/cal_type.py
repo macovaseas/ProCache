def45b46c059a416cd3f1f8c69e87f51706751d4
macovaseas <104780740+macovaseas@users.noreply.github.com>
2026-03-19 23:53:16 +0800

    Update code

--- DiT-ProCache/cache_functions/cal_type.py
+++ DiT-ProCache/cache_functions/cal_type.py
@@ -1,34 +1,12 @@
 s = [1, 0, 2, 0, 1, 0, 2, 0, 1, 0, 2, 0, 1, 0, 2, 0, 1, 0, 2, 0, 1, 0, 2, 0, 1, 0, 2, 1, 0, 2, 1, 0, 2, 1, 0, 2, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0]
 
 
-# def cal_type(cache_dic, current):
-#     '''
-#     Determine calculation type for this step
-#     '''
-#     last_steps = (current['step'] <=2)
-#     first_step = (current['step'] == (current['num_steps'] - 1))
-#     force_fresh = cache_dic['force_fresh']
-#     if not first_step:
-#         fresh_interval = cache_dic['cal_threshold']
-#     else:
-#         fresh_interval = cache_dic['fresh_threshold']
-
-#     if (current['step'] % fresh_interval == 0) or first_step:
-#         current['type'] = 'full'
-        
-#     elif ((current['step'] % fresh_interval) % 2 == 1): #[1,3,5] [2,4,6]
-#         current['type'] = 'ToCa'
-#     # 'ToCa' 'FORA'
-#     else: 
-#         current['type'] = 'ToCa'
-
-
 def cal_type(cache_dic, current):
     '''
     Determine calculation type for this step
     '''
     first_step = (current['step'] == (current['num_steps'] - 1))    
-    step_index = current['step']
+    step_index = 49 - current['step']
     is_cache_step = s[step_index]
     threshold = 21
     
